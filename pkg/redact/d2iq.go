d9e2734acc64e5a921d013017085875057841c90
Jabar Asadi <azhovan@outlook.com>
2022-08-15 15:07:16 +0200

    feat: add missing file after upstream sync

---
+++ pkg/redact/d2iq.go
@@ -0,0 +1,7 @@
+package redact
+
+// BuildAdditionalRedactors is used in dkp-diagnostics pkg
+var BuildAdditionalRedactors = buildAdditionalRedactors
+
+// DefaultRedactors is used in dkp-diagnostics pkg
+var DefaultRedactors = getRedactors
